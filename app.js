f920dbedbe28c95240d2f7f09a32e57168020211
Ephrem Demelash <demelashephrem@gmail.com>
2020-11-23 21:24:53 +0300

    🐛 FIX: transaction routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var txsRouter = require('./routes/transactions');
 
 var app = express();
 
@@ -16,6 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/txs', txsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
